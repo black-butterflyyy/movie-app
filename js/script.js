c71100da7792709e87e427bf49d876115e17a433
Hosna <blackbutterfllly@gmail.com>
2023-11-29 13:44:53 +0330

    Add display popular movies for homepage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,55 @@ const state = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = ` 
+          <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path
+              ? `  <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+              : `  <img
+              src="/images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+          }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+            <p class="card-text">
+                <small class="text-muted">Vote: ${movie.vote_average}</small>
+            </p>
+            
+          </div>
+        `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Fetch data form TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '9c50fc473f517bbfab571ebd19f1c033';
+  const API_URL = 'https://api.themoviedb.org/3/';
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+  return data;
+}
+
 // Highligh active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -19,7 +68,7 @@ function init() {
   switch (state.currentPage) {
     case '/':
     case '/index.html':
-      console.log('HOme');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('shows');
