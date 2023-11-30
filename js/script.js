503d31b4cc13b555e82d1980aa619dc344a7a872
Hosna <blackbutterfllly@gmail.com>
2023-11-30 12:34:54 +0330

    Add display show details function

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ async function displayPopularShows() {
 // Display movie details
 async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
-  const movie = await fetchAPIData(`movie/${movieId}`);
+  const movie = await fetchAPIData(`show/${movieId}`);
 
   // Overly for background image
   displayBackgroundImage('movie', movie.backdrop_path);
@@ -144,6 +144,75 @@ async function displayMovieDetails() {
         </div>`;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display show details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overly for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+          <div>
+             ${
+               show.poster_path
+                 ? `  <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                 : `  <img
+              src="/images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+             }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Seasons: </span> ${
+              show.number_of_seasons
+            }</li>
+             <li><span class="text-secondary">Number Of Episodes: </span> ${
+               show.number_of_episodes
+             }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air: </span>  ${
+                show.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies
+            .map((company) => `<span>${company.name}</span>`)
+            .join(', ')}</div>
+        </div>`;
+
+  document.querySelector('#show-details').appendChild(div);
 }
 
 // Display backdrop for pages
@@ -221,7 +290,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('tv details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('search');
