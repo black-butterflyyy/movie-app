ba82363e7ba18f179a3ed5803f7eb273442a388c
Hosna <blackbutterfllly@gmail.com>
2023-12-02 14:50:10 +0330

    Refactor display background image function

--- js/script.js
+++ js/script.js
@@ -231,23 +231,14 @@ async function displayShowDetails() {
 
 // Display backdrop for pages
 function displayBackgroundImage(type, backgroundPath) {
-  const overlayDiv = document.createElement('div');
-  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
-  overlayDiv.style.backgroundSize = 'cover';
-  overlayDiv.style.backgroundPosition = 'center';
-  overlayDiv.style.backgroundRepeat = 'no-repeat';
-  overlayDiv.style.height = '100vh';
-  overlayDiv.style.width = '100vw';
-  overlayDiv.style.position = 'absolute';
-  overlayDiv.style.top = '0';
-  overlayDiv.style.left = '0';
-  overlayDiv.style.zIndex = '-1';
-  overlayDiv.style.opacity = '0.3';
+  const backdropDiv = document.createElement('div');
+  backdropDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  backdropDiv.classList.add('backdrop');
 
   if (type === 'movie') {
-    document.querySelector('#movie-details').appendChild(overlayDiv);
+    document.querySelector('#movie-details').appendChild(backdropDiv);
   } else {
-    document.querySelector('#show-details').appendChild(overlayDiv);
+    document.querySelector('#show-details').appendChild(backdropDiv);
   }
 }
 
