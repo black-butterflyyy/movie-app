fa260a9efa61a0fed08b5f21d180620ef1339c00
Hosna <blackbutterfllly@gmail.com>
2023-12-02 10:19:38 +0330

    Fix shows router for production

--- js/script.js
+++ js/script.js
@@ -409,6 +409,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
+    case '/shows':
       displayPopularShows();
       break;
     case '/movie-details.html':
